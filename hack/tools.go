1b90f1598e1396a336bc5980b62a216cfb1558d9
Luca Terracciano <terraccianoluca@gmail.com>
2021-06-23 15:29:27 +0200

    fix: add missing build tools

--- hack/tools.go
+++ hack/tools.go
@@ -1,6 +1,8 @@
+// +build tools
+
 package tools
 
-// import (
-// 	_ "k8s.io/code-generator" // This package imports things required by build scripts, to force `go mod` to see them as dependencies
-// 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
-// )
+import (
+	_ "k8s.io/code-generator" // This package imports things required by build scripts, to force `go mod` to see them as dependencies
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+)
