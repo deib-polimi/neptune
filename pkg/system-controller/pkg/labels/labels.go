b7f838db5f3bcf6786c47f9140cda30e7d6e3657
Luca Terracciano <terraccianoluca@gmail.com>
2021-07-12 16:53:30 +0200

    refactor: change label generation

--- pkg/system-controller/pkg/labels/labels.go
+++ pkg/system-controller/pkg/labels/labels.go
@@ -12,18 +12,20 @@ const (
 	Leader CommunityRole = "LEADER"
 	Member CommunityRole = "MEMBER"
 	// CommunityRoleLabel identifies a role of a node inside a community
-	CommunityRoleLabel CommunityRole = "edgeautoscaler.polimi.it.role"
+	CommunityRoleLabel CommunityRole = "edgeautoscaler.polimi.it/role"
 
 	// CommunityLabel defines the community a node belongs to
-	CommunityLabel Community = "edgeautoscaler.polimi.it.community"
+	CommunityLabel Community = "edgeautoscaler.polimi.it/community"
 
 	// MasterNodeLabel is the label used by Kubernetes to specify the node running the control plane
 	MasterNodeLabel string = "node-role.kubernetes.io/master"
 )
 
 func (c Community) WithNamespace(ns string) Community {
-	prefix := strings.Split(c.String(), "/")[0]
-	return Community(fmt.Sprintf("%s/%s", prefix, ns))
+	parts := strings.Split(c.String(), "/")
+	prefix := parts[0]
+	suffix := strings.Split(parts[1], ".")[0]
+	return Community(fmt.Sprintf("%s/%s.%s", prefix, ns, suffix))
 }
 
 func (c Community) String() string {
@@ -31,8 +33,10 @@ func (c Community) String() string {
 }
 
 func (c CommunityRole) WithNamespace(ns string) CommunityRole {
-	prefix := strings.Split(c.String(), "/")[0]
-	return CommunityRole(fmt.Sprintf("%s/%s", prefix, ns))
+	parts := strings.Split(c.String(), "/")
+	prefix := parts[0]
+	suffix := strings.Split(parts[1], ".")[0]
+	return CommunityRole(fmt.Sprintf("%s/%s.%s", prefix, ns, suffix))
 }
 
 func (c CommunityRole) String() string {
